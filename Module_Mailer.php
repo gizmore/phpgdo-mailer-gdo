41f3e4eadb956a1765b0125674daf5968502d16f
gizmore <gizmore@wechall.net>
2022-05-07 00:16:09 +0200

    7.0.0-r1005

---
+++ Module_Mailer.php
@@ -0,0 +1,19 @@
+<?php
+namespace GDO\Mailer;
+
+use GDO\Core\GDO_Module;
+
+/**
+ * Mailer module that can send Mail.
+ * 
+ * @see Mail
+ * 
+ * @author gizmore
+ * @version 7.0.0
+ * @since 7.0.0
+ * @license GDOv7-LICENSE
+ */
+final class Module_Mailer extends GDO_Module
+{
+	
+}
